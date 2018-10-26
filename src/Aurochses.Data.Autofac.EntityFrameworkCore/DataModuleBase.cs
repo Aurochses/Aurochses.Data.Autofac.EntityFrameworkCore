3fab863c93e6e7d40ff94d75b07068a3f2898ed8
Vladimir Rodchenko <vladimir@rodchenko.by>
2018-10-26 17:34:14 +0300

    added autofac modules

---
+++ src/Aurochses.Data.Autofac.EntityFrameworkCore/DataModuleBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Aurochses.Data.Autofac.EntityFrameworkCore
+{
+    /// <summary>
+    /// Autofac module for data layer
+    /// </summary>
+    /// <seealso cref="Module" />
+    public class DataModuleBase : global::Autofac.Module
+    {
+        
+    }
+}
